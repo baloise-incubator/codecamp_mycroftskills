36b726a2895401f01f2e2a387a644d7c723bbc7a
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2021-11-02 15:08:24 +0100

    added remove operation

--- einkaufsliste-skill/einkaufsliste_persistence.py
+++ einkaufsliste-skill/einkaufsliste_persistence.py
@@ -4,12 +4,25 @@ from google.cloud import firestore
 class persistence:
 
     def createItem(self, item):
-        db = firestore.Client(project='voice-assistant-330820') #Client.from_service_account_json('/Users/lukasbrendle/Downloads/voice-assistant-330820-c4dbf6b3a7e9.json')
+        db = firestore.Client.from_service_account_json('./voice-assistant-330820-c4dbf6b3a7e9.json')
         doc_ref = db.collection(u'Einkaufsliste').document(f'{item}')
-        amount = doc_ref.get({'amount'}).get('amount')
+        amount = int(doc_ref.get({'amount'}).get('amount') or 0)
         doc_ref.set({
-            u'amount': f'{int(amount or 0) + 1}',
+            u'amount': f'{amount + 1}',
         })
+        return doc_ref.id + " " + doc_ref.get({'amount'}).get('amount')
+
+    def removeItem(self, item):
+        db = firestore.Client.from_service_account_json('./voice-assistant-330820-c4dbf6b3a7e9.json')
+        doc_ref = db.collection(u'Einkaufsliste').document(f'{item}')
+        amount = int(doc_ref.get({'amount'}).get('amount') or 0)
+        if amount != 0:
+            doc_ref.set({
+                u'amount': f'{amount + 1}',
+            })
+        else:
+            return "fail"
+
         return doc_ref.id + " " + doc_ref.get({'amount'}).get('amount')
 
 if __name__ == '__main__':
