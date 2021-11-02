a640dfcdd51bdd75428ccd46ec650b34c113793c
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2021-11-02 14:52:16 +0100

    incrementing works

--- einkaufsliste-skill/einkaufsliste_persistence.py
+++ einkaufsliste-skill/einkaufsliste_persistence.py
@@ -6,12 +6,11 @@ class persistence:
     def createItem(self, item):
         db = firestore.Client(project='voice-assistant-330820') #Client.from_service_account_json('/Users/lukasbrendle/Downloads/voice-assistant-330820-c4dbf6b3a7e9.json')
         doc_ref = db.collection(u'Einkaufsliste').document(f'{item}')
-        #print(doc_ref.get({
-        #    u'amount'}).)
+        amount = doc_ref.get({'amount'}).get('amount')
         doc_ref.set({
-            u'amount': f'{1}',
+            u'amount': f'{int(amount or 0) + 1}',
         })
-        return doc_ref.id
+        return doc_ref.id + " " + doc_ref.get({'amount'}).get('amount')
 
 if __name__ == '__main__':
     persistence = persistence()
