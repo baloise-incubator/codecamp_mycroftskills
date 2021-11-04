42112644204937583ba572941faca44f64030684
ProjectBO <berkay_gueltekin@hotmail.com>
2021-11-04 17:30:40 +0100

    wecker

--- erinnerung-skill/__init__.py
+++ erinnerung-skill/__init__.py
@@ -5,14 +5,12 @@ class Erinnerung(MycroftSkill):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('erinnerung.intent')
-    def handle_erinnerung(self, zeit):
-        timedict = {
-            "Zeit": zeit
-        }
-        self.schedule_event(self.wecker, timedict)
+    def handle_erinnerung(self, message):
+        zeit = message.data.get('zeit')
+        self.schedule_event(self.wecker, int(zeit), zeit)
 
-    def wecker(self):
-        self.speak(timedict)
+    def wecker(self, message):
+        self.speak(f'{message.data}' + ' sind um')
 
 def create_skill():
     return Erinnerung()
