6a9ac085b9599c6150d98ea83fa222e78c8de434
Lukas Brendle <lukas.a.j.brendle@gmail.com>
2021-11-04 13:26:04 +0100

    skill added

---
+++ hellothere_skill/README.md
@@ -0,0 +1,9 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/>Hello_There
+You are a bold one
+## About
+This skill is a experimental skill for testing capabilites of the mycroft framework
+
+## Examples
+* "Hello there"
+
+
---
+++ hellothere_skill/__init__.py
@@ -0,0 +1,20 @@
+from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.parse import extract_number, match_one, extract_datetime
+from mycroft.skills.audioservice import AudioService
+
+
+class HelloThereSkill(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        self.audio_service = AudioService(self.bus)
+
+    location = 'https://www.thesoundarchive.com/play-wav-files.asp?sound=starwars/disturbence.mp3'
+
+    @intent_file_handler('hello_there.intent')
+    def handle_hello_there(self, message):
+        self.audio_service.play(self.location)
+        self.speak('und tschüss')
+
+
+def create_skill():
+    return HelloThereSkill()
