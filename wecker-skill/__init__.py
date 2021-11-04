df38a9d3fb36eebc37503770c77233f312995a00
Berkay Gueltekin <berkay.gueltekin@baloise.ch>
2021-11-04 18:28:39 +0100

    add time conversion

--- wecker-skill/__init__.py
+++ wecker-skill/__init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util.parse import extract_number, match_one, extract_datetime
 
 class Wecker(MycroftSkill):
     def __init__(self):
@@ -6,8 +7,19 @@ class Wecker(MycroftSkill):
 
     @intent_file_handler('wecker.intent')
     def handle_wecker(self, message):
+        
+        zahl = int(extract_number(message.data.get('zahl'), lang='de-de'))
         zeit = message.data.get('zeit')
-        self.schedule_event(self.wecker, int(zeit), zeit)
+        if zeit == 'Stunden':
+            zahlSekunden = getSekundenVonStunden(zahl)
+        elif zeit == 'Minuten' or zeit == 'Minute':
+            zahlSekunden = getSekundenVonMinuten(zahl)
+        else:
+            zahlSekunden = zahl
+            
+        elementFürDenSatz = str(zahl) + ' ' + zeit
+            
+        self.schedule_event(self.wecker, int(zahlSekunden), elementFürDenSatz)
 
     def wecker(self, message):
         self.speak(f'{message.data}' + ' sind um')
@@ -15,3 +27,8 @@ class Wecker(MycroftSkill):
 def create_skill():
     return Wecker()
 
+def getSekundenVonMinuten(zahl):
+    return zahl * 60
+
+def getSekundenVonStunden(zahl):
+    return zahl * 3600
